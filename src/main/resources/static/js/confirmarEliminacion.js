d0e4dcda7fc9bb7643b6d708b580bbbcb7cdfa5a
JRodriAlarc <jona10131313@gmail.com>
2024-07-09 13:28:29 -0600

    Verificar si la imagen existe para mostarla en el Modal de eliminar Prodcutos

--- src/main/resources/static/js/confirmarEliminacion.js
+++ src/main/resources/static/js/confirmarEliminacion.js
@@ -1,10 +1,17 @@
 let productIdToDelete;
 
 function confirmDelete(productId, nombre, imagen) {
+    console.log('Valor de imagen:', imagen); // Verificar qué valor tiene imagen
+
     productIdToDelete = productId;
     document.getElementById('eliminar_producto').classList.remove('hidden');
     document.getElementById('nombre_producto').textContent = nombre;
-    document.getElementById('imagen_producto').src = imagen;
+    // Verificar si la imagen está definida y no es null
+    if (imagen && imagen.trim() !== '' && imagen !== 'null') {
+        document.getElementById('imagen_producto').src = imagen;
+    } else {
+        document.getElementById('imagen_producto').src = '/img/producto-sin-imagen.png';
+    }
 }
 
 document.getElementById('confirmDeleteBtn').addEventListener('click', function() {
