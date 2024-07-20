ed54a320de6a0bea9255439f2ebb204a95804781
JRodriAlarc <jona10131313@gmail.com>
2024-07-20 00:21:10 -0600

    Eliminar Redundancia de la alerta de login por Usuario o Password Incorrecto

--- src/main/resources/static/js/alertaErrorLogin.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var alertElement = document.getElementById('alert-border-2');
-    if (!alertElement.classList.contains('hidden')) {
-        alertElement.classList.remove('hidden');
-    }
-});
