767c594476f911cbbeff83abacf9f94d8e9e2de5
Aldo-Antonio-27 <aldoantoniobadillovega@gmail.com>
2024-07-21 17:33:28 -0600

    Modificación de la Manera de Generar los Campos Dinámicos

--- src/main/resources/static/js/agregarProductos.js
+++ src/main/resources/static/js/agregarProductos.js
@@ -1,43 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const opcionesCategorias = document.getElementById('opciones_categoria_agregar_producto');
-  const opcionSeleccionada = document.getElementById('opcion_seleccionada_categoria_agregar_producto');
-  const spanCategoria = opcionSeleccionada.querySelector('#span_categoria');
-  const categorias = opcionesCategorias.querySelectorAll('div[data-value]');
-  const contenedor = document.getElementById('campos_dinamicos_agregar_productos');
+  const opcionesCategoria = document.getElementById('opciones_categoria_agregar_producto');
+  const spanCategoria = document.getElementById('span_categoria');
+  const selectCategoria = document.getElementById('valores_categoria_agregar_producto');
 
-  categorias.forEach(function (opcion) {
-    opcion.addEventListener('click', function () {
-      const valor = this.getAttribute('data-value');
-      spanCategoria.textContent = this.textContent;
-      spanCategoria.setAttribute('data-value', valor);
-      contenedor.innerHTML = '';
-
-      if (valor === 'elasticos') {
-        camposElasticos();
-        // Lanza un evento global para indicar que se han generado elementos
-        const eventoCamposGenerados = new CustomEvent('elementosGenerados', { detail: { categoria: 'elasticos' } });
-          document.dispatchEvent(eventoCamposGenerados);
-      } else if (valor === 'hilos') {
-        camposHilos();
-        // Lanza un evento global para indicar que se han generado elementos
-        const eventoCamposGenerados = new CustomEvent('elementosGenerados', { detail: { categoria: 'hilos' } });
-          document.dispatchEvent(eventoCamposGenerados);
-      } else if (valor === 'cordones') {
-        camposCordones();
-        // Lanza un evento global para indicar que se han generado elementos
-        const eventoCamposGenerados = new CustomEvent('elementosGenerados', { detail: { categoria: 'cordones' } });
-          document.dispatchEvent(eventoCamposGenerados);
-      } else if (valor === 'botones') {
-        camposBotones();
-        // Lanza un evento global para indicar que se han generado elementos
-        const eventoCamposGenerados = new CustomEvent('elementosGenerados', { detail: { categoria: 'botones' } });
-          document.dispatchEvent(eventoCamposGenerados);
-      }
-    });
+  // Funcionalidad para detectar el onchange
+  opcionesCategoria.addEventListener('click', function(event) {
+    const target = event.target;
+    if (target.hasAttribute('data-value')) {
+      const value = target.getAttribute('data-value');
+      const text = target.textContent.trim();
+      spanCategoria.textContent = text;
+      selectCategoria.value = value;
+      selectCategoria.dispatchEvent(new Event('change')); 
+    }
   });
-  
-  // Campos Dinamicos Elasticos
-  function camposElasticos() {
+});
+
+// Campos Dinamicos
+function categoriaDinamica() {
+  const contenedor = document.getElementById('campos_dinamicos_agregar_productos');
+  const selectCategoria = document.getElementById('valores_categoria_agregar_producto');
+  const valor = selectCategoria.value;
+
+  if (valor === 'elasticos') {
     contenedor.innerHTML = `
       <div class="px-4 md:px-12 flex justify-center">
         <div class="grid grid-cols-12 start-0 flex items-stretch mt-4 md:mt-4 lg:mt-4 xl:mt-5 px-4 w-full">
@@ -59,14 +44,14 @@ document.addEventListener('DOMContentLoaded', function() {
           </div>
         </div>
       </div>
-
+    
       <div class="px-4 md:px-12 flex justify-center">
         <div class="grid grid-cols-12 start-0 flex items-stretch mt-4 md:mt-4 lg:mt-4 xl:mt-5 px-4 w-full">
           <p class="col-span-5 text-center fuente_2 text-lg md:text-xl lg:text-xl xl:text-2xl text-white pr-6 place-self-center justify-self-end">Longitud:</p>
           <input type="number" id="longitud_elasticos" th:field="*{cantidad}" class="col-span-7 place-self-center justify-self-start fuente_3 bg-[#db4900] border border-[#db4900] text-white text-base md:text-lg rounded-3xl focus:ring-[#db4900] focus:border-[#db4900] block w-full p-1.5 px-3 md:p-1.5 md:px-3 lg:p-1.5 lg:px-3 xl:p-2 xl:px-3" required />
         </div>
       </div>
-
+    
       <div class="px-4 md:px-12 flex justify-center">
         <div class="grid grid-cols-12 start-0 flex items-stretch mt-4 md:mt-4 lg:mt-4 xl:mt-5 px-4 w-full">
           <p class="col-span-5 text-center fuente_2 text-lg md:text-xl lg:text-xl xl:text-2xl text-white pr-6 place-self-center justify-self-end">Color:</p>
@@ -90,10 +75,12 @@ document.addEventListener('DOMContentLoaded', function() {
         </div>
       </div>
     `;
-  }
 
-  // Campos Dinamicos Hilos
-  function camposHilos() {
+    // Lanza un evento global para indicar que se han generado elementos
+    const eventoCamposGenerados = new CustomEvent('elementosGenerados', { detail: { categoria: 'elasticos' } });
+    document.dispatchEvent(eventoCamposGenerados);
+
+  } else if (valor === 'hilos') {
     contenedor.innerHTML = `
       <div class="px-4 md:px-12 flex justify-center">
         <div class="grid grid-cols-12 start-0 flex items-stretch mt-4 md:mt-4 lg:mt-4 xl:mt-5 px-4 w-full">
@@ -119,7 +106,7 @@ document.addEventListener('DOMContentLoaded', function() {
           </div>
         </div>
       </div>
-
+    
       <div class="px-4 md:px-12 flex justify-center">
         <div class="grid grid-cols-12 start-0 flex items-stretch mt-4 md:mt-4 lg:mt-4 xl:mt-5 px-4 w-full">
           <p class="col-span-5 text-center fuente_2 text-lg md:text-xl lg:text-xl xl:text-2xl text-white pr-6 place-self-center justify-self-end">Color:</p>
@@ -146,7 +133,7 @@ document.addEventListener('DOMContentLoaded', function() {
           </div>
         </div>
       </div>
-
+    
       <div class="px-4 md:px-12 flex justify-center">
         <div class="grid grid-cols-12 start-0 flex items-stretch mt-4 md:mt-4 lg:mt-4 xl:mt-5 px-4 w-full">
           <p class="col-span-5 text-center fuente_2 text-lg md:text-xl lg:text-xl xl:text-2xl text-white pr-6 place-self-center justify-self-end">Tapa:</p>
@@ -168,10 +155,12 @@ document.addEventListener('DOMContentLoaded', function() {
         </div>
       </div>
     `;
-  }
 
-  // Campos Dinamicos Cordones
-  function camposCordones() {
+    // Lanza un evento global para indicar que se han generado elementos
+    const eventoCamposGenerados = new CustomEvent('elementosGenerados', { detail: { categoria: 'hilos' } });
+    document.dispatchEvent(eventoCamposGenerados);
+
+  } else if (valor === 'cordones') {
     contenedor.innerHTML = `
       <div class="px-4 md:px-12 flex justify-center">
         <div class="grid grid-cols-12 start-0 flex items-stretch mt-4 md:mt-4 lg:mt-4 xl:mt-5 px-4 w-full">
@@ -179,7 +168,7 @@ document.addEventListener('DOMContentLoaded', function() {
           <input type="number" id="longitud_cordones" th:field="*{cantidad}" class="col-span-7 place-self-center justify-self-start fuente_3 bg-[#db4900] border border-[#db4900] text-white text-base md:text-lg rounded-3xl focus:ring-[#db4900] focus:border-[#db4900] block w-full p-1.5 px-3 md:p-1.5 md:px-3 lg:p-1.5 lg:px-3 xl:p-2 xl:px-3" required />
         </div>
       </div>
-
+    
       <div class="px-4 md:px-12 flex justify-center">
         <div class="grid grid-cols-12 start-0 flex items-stretch mt-4 md:mt-4 lg:mt-4 xl:mt-5 px-4 w-full">
           <p class="col-span-5 text-center fuente_2 text-lg md:text-xl lg:text-xl xl:text-2xl text-white pr-6 place-self-center justify-self-end">Color:</p>
@@ -211,10 +200,12 @@ document.addEventListener('DOMContentLoaded', function() {
         </div>
       </div>
     `;
-  }
 
-  // Campos Dinamicos Botones
-  function camposBotones() {
+    // Lanza un evento global para indicar que se han generado elementos
+    const eventoCamposGenerados = new CustomEvent('elementosGenerados', { detail: { categoria: 'cordones' } });
+    document.dispatchEvent(eventoCamposGenerados);
+
+  } else if (valor === 'botones') {
     contenedor.innerHTML = `
       <div class="px-4 md:px-12 flex justify-center">
         <div class="grid grid-cols-12 start-0 flex items-stretch mt-4 md:mt-4 lg:mt-4 xl:mt-5 px-4 w-full">
@@ -228,7 +219,7 @@ document.addEventListener('DOMContentLoaded', function() {
               <div class="px-4 py-2 text-white hover:bg-[#da6930] cursor-pointer" data-value="niquelado">Niquelado</div>
               <div class="px-4 py-2 text-white hover:bg-[#da6930] cursor-pointer" data-value="pasta">Pasta</div>
             </div>
-            <select id="valores_tipo_agregar_producto_botones" th:field="*{color}" class="hidden" required>
+            <select id="valores_tipo_agregar_producto_botones" onchange="colorDinamico()" th:field="*{color}" class="hidden" required>
               <option value="" selected>Seleccionar:</option>
               <option value="niquelado">Niquelado</option>
               <option value="pasta">Pasta</option>
@@ -237,40 +228,57 @@ document.addEventListener('DOMContentLoaded', function() {
         </div>
       </div>
 
-      <div id="opcion_1" class="px-4 md:px-12 flex justify-center">
+      <div id="campos_dinamicos_color_botones">
+                                
+      </div>
+    `;
+
+    const opcionesCategoriaTipoBotones = document.getElementById('opciones_tipo_agregar_producto_botones');
+    const spanCategoriaTipoBotones = document.getElementById('span_categoria_tipo_botones');
+    const selectCategoriaTipoBotones = document.getElementById('valores_tipo_agregar_producto_botones');
+    
+    // Funcionalidad para detectar el onchange tipo de botones
+    opcionesCategoriaTipoBotones.addEventListener('click', function(event) {
+      const target = event.target;
+      if (target.hasAttribute('data-value')) {
+        const valueTipoBotones = target.getAttribute('data-value');
+        const textTipoBotones = target.textContent.trim();
+        spanCategoriaTipoBotones.textContent = textTipoBotones;
+        selectCategoriaTipoBotones.value = valueTipoBotones;
+        selectCategoriaTipoBotones.dispatchEvent(new Event('change')); 
+      }
+    });
+    
+    // Lanza un evento global para indicar que se han generado elementos
+    const eventoCamposGenerados = new CustomEvent('elementosGenerados', { detail: { categoria: 'botones' } });
+    document.dispatchEvent(eventoCamposGenerados);
+  }
+}
+
+// Campos Dinamicos Color Botones
+function colorDinamico() {
+  const contenedorColorDinamico = document.getElementById('campos_dinamicos_color_botones');
+  const selectTipoBotones = document.getElementById('valores_tipo_agregar_producto_botones');
+  const valorTipoBotones = selectTipoBotones.value;
+
+  if (valorTipoBotones === 'niquelado') {
+    contenedorColorDinamico.innerHTML = `
+      <div class="px-4 md:px-12 flex justify-center">
         <div class="grid grid-cols-12 start-0 flex items-stretch mt-4 md:mt-4 lg:mt-4 xl:mt-5 px-4 w-full">
           <p class="col-span-5 text-center fuente_2 text-lg md:text-xl lg:text-xl xl:text-2xl text-white pr-6 place-self-center justify-self-end">Color:</p>
           <div class="relative col-span-7 place-self-center justify-self-start fuente_3 w-full">
-            <div id="opcion_seleccionada_color_opcion_1_agregar_producto_botones" class="bg-[#db4900] text-white border border-[#da6930] focus:ring-[#da6930] focus:border-[#da6930] font-medium rounded-3xl text-sm md:text-base lg:text-base xl:text-lg fuente_2 w-full p-1.5 md:p-1.5 lg:p-1.5 xl:p-2 text-left cursor-pointer flex justify-between items-center">
+            <div id="opcion_seleccionada_color_agregar_producto_botones" class="bg-[#db4900] text-white border border-[#da6930] focus:ring-[#da6930] focus:border-[#da6930] font-medium rounded-3xl text-sm md:text-base lg:text-base xl:text-lg fuente_2 w-full p-1.5 md:p-1.5 lg:p-1.5 xl:p-2 text-left cursor-pointer flex justify-between items-center">
               <span class="px-1">Seleccionar:</span>
               <svg class="w-6 h-6 md:w-7 md:h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m7 10l5 5l5-5"/></svg>
             </div>
-            <div id="opciones_color_opcion_1_agregar_producto_botones" class="absolute hidden bg-[#db4900] border border-[#da6930] w-full text-sm md:text-base lg:text-base xl:text-base fuente_2 mt-1 z-10 max-h-48 overflow-y-auto">
+            <div id="opciones_color_agregar_producto_botones" class="absolute hidden bg-[#db4900] border border-[#da6930] w-full text-sm md:text-base lg:text-base xl:text-base fuente_2 mt-1 z-10 max-h-48 overflow-y-auto">
               <div class="px-4 py-2 text-white hover:bg-[#da6930] cursor-pointer" data-value="zinc_viejo">Zinc Viejo</div>
               <div class="px-4 py-2 text-white hover:bg-[#da6930] cursor-pointer" data-value="niquelado_viejo">Niquelado Viejo</div>
             </div>
-            <select id="valores_color_opcion_1_agregar_producto_botones" th:field="*{color}" class="hidden" required>
+            <select id="valores_color_agregar_producto_botones" th:field="*{color}" class="hidden" required>
               <option value="" selected>Seleccionar:</option>
               <option value="zinc_viejo">Zinc Viejo</option>
               <option value="niquelado_viejo">Niquelado Viejo</option>
-            </select>
-          </div>
-        </div>
-      </div>
-    
-      <div id="opcion_2" class="hidden px-4 md:px-12 flex justify-center">
-        <div class="grid grid-cols-12 start-0 flex items-stretch mt-4 md:mt-4 lg:mt-4 xl:mt-5 px-4 w-full">
-          <p class="col-span-5 text-center fuente_2 text-lg md:text-xl lg:text-xl xl:text-2xl text-white pr-6 place-self-center justify-self-end">Color:</p>
-          <div class="relative col-span-7 place-self-center justify-self-start fuente_3 w-full">
-            <div id="opcion_seleccionada_color_opcion_2_agregar_producto_botones" class="bg-[#db4900] text-white border border-[#da6930] focus:ring-[#da6930] focus:border-[#da6930] font-medium rounded-3xl text-sm md:text-base lg:text-base xl:text-lg fuente_2 w-full p-1.5 md:p-1.5 lg:p-1.5 xl:p-2 text-left cursor-pointer flex justify-between items-center">
-              <span class="px-1">Transparente</span>
-              <svg class="w-6 h-6 md:w-7 md:h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m7 10l5 5l5-5"/></svg>
-            </div>
-            <div id="opciones_color_opcion_2_agregar_producto_botones" class="absolute hidden bg-[#db4900] border border-[#da6930] w-full text-sm md:text-base lg:text-base xl:text-base fuente_2 mt-1 z-10 max-h-48 overflow-y-auto">
-              <div class="px-4 py-2 text-white hover:bg-[#da6930] cursor-pointer" data-value="transparente">Transparente</div>
-            </div>
-            <select id="valores_color_opcion_2_agregar_producto_botones" th:field="*{color}" class="hidden" required>
-              <option value="transparente" selected>Transparente</option>
             </select>
           </div>
         </div>
@@ -298,27 +306,43 @@ document.addEventListener('DOMContentLoaded', function() {
       </div>
     `;
 
-    const opcionesCategoriasTipoBotones = document.getElementById('opciones_tipo_agregar_producto_botones');
-    const opcionSeleccionadaTipoBotones = document.getElementById('opcion_seleccionada_tipo_agregar_producto_botones');
-    const spanCategoriaTipoBotones = opcionSeleccionadaTipoBotones.querySelector('#span_categoria_tipo_botones');
-    const categoriasTipoBotones = opcionesCategoriasTipoBotones.querySelectorAll('div[data-value]');
-    const opcion_1 = document.getElementById('opcion_1');
-    const opcion_2 = document.getElementById('opcion_2');
-    
-    categoriasTipoBotones.forEach(function (opcion) {
-      opcion.addEventListener('click', function () {
-        const valorTipoBotones = this.getAttribute('data-value');
-        spanCategoriaTipoBotones.textContent = this.textContent;
-        spanCategoriaTipoBotones.setAttribute('data-value', valorTipoBotones);
+    // Lanza un evento global para indicar que se han generado elementos
+    const eventoCamposGenerados = new CustomEvent('elementosGenerados', { detail: { categoria: 'botonesColorDinamicoOpcion1' } });
+    document.dispatchEvent(eventoCamposGenerados);
 
-        if(valorTipoBotones == "pasta"){
-          opcion_1.classList.add('hidden');
-          opcion_2.classList.remove('hidden');
-        } else{
-          opcion_1.classList.remove('hidden');
-          opcion_2.classList.add('hidden');
-        }
-      });
-    });
+  } else if (valorTipoBotones === 'pasta') {
+    contenedorColorDinamico.innerHTML = `
+      <div class="px-4 md:px-12 flex justify-center">
+        <div class="grid grid-cols-12 start-0 flex items-stretch mt-4 md:mt-4 lg:mt-4 xl:mt-5 px-4 w-full">
+          <p class="col-span-5 text-center fuente_2 text-lg md:text-xl lg:text-xl xl:text-2xl text-white pr-6 place-self-center justify-self-end">Color:</p>
+          <input type="text" id="nombre" value="Transparente" disabled th:field="*{nombre}" class="col-span-7 place-self-center justify-self-start fuente_3 bg-[#db4900] border border-[#db4900] text-white text-base md:text-base lg:text-base xl:text-lg rounded-3xl focus:ring-[#db4900] focus:border-[#db4900] block w-full p-1.5 px-2 md:p-2 md:px-3 lg:p-2 lg:px-3 xl:p-2 xl:px-3" required />
+        </div>
+      </div>
+          
+      <div class="px-4 md:px-12 flex justify-center">
+        <div class="grid grid-cols-12 start-0 flex items-stretch mt-4 md:mt-4 lg:mt-4 xl:mt-5 px-4 w-full">
+          <p class="col-span-5 text-center fuente_2 text-lg md:text-xl lg:text-xl xl:text-2xl text-white pr-6 place-self-center justify-self-end">Tamaño:</p>
+          <div class="relative col-span-7 place-self-center justify-self-start fuente_3 w-full">
+            <div id="opcion_seleccionada_tamano_agregar_producto_botones" class="bg-[#db4900] text-white border border-[#da6930] focus:ring-[#da6930] focus:border-[#da6930] font-medium rounded-3xl text-sm md:text-base lg:text-base xl:text-lg fuente_2 w-full p-1.5 md:p-1.5 lg:p-1.5 xl:p-2 text-left cursor-pointer flex justify-between items-center">
+              <span class="px-1">Seleccionar:</span>
+              <svg class="w-6 h-6 md:w-7 md:h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m7 10l5 5l5-5"/></svg>
+            </div>
+            <div id="opciones_tamano_agregar_producto_botones" class="absolute hidden bg-[#db4900] border border-[#da6930] w-full text-sm md:text-base lg:text-base xl:text-base fuente_2 mt-1 z-10 max-h-48 overflow-y-auto">
+              <div class="px-4 py-2 text-white hover:bg-[#da6930] cursor-pointer" data-value="27_l">27 L</div>
+              <div class="px-4 py-2 text-white hover:bg-[#da6930] cursor-pointer" data-value="30_l">30 L</div>
+            </div>
+            <select id="valores_tamano_agregar_producto_botones" th:field="*{color}" class="hidden" required>
+              <option value="" selected>Seleccionar:</option>
+              <option value="27_l">27 L</option>
+              <option value="30_l">30 L</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    `;
+
+    // Lanza un evento global para indicar que se han generado elementos
+    const eventoCamposGenerados = new CustomEvent('elementosGenerados', { detail: { categoria: 'botonesColorDinamicoOpcion2' } });
+    document.dispatchEvent(eventoCamposGenerados);
   }
-});
+}
