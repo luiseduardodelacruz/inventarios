65306247ad1ce995e6d33a3fb3bb1033dcbf1815
JRodriAlarc <jona10131313@gmail.com>
2024-07-22 14:49:49 -0600

    Ajuste al Formualrio de Pedido, eliminación del modo obscuro

--- js/pedido.js
+++ js/pedido.js
@@ -1,15 +1,10 @@
 //carrusel de imagenes del select 
-
 const container = document.getElementById('imageContainer');
 const numImagesSelect = document.getElementById('numImagesSelect');
 
-
 numImagesSelect.addEventListener('change', function(event) {
     event.preventDefault();
-    
-
     const numImages = parseInt(event.target.value);
-    
    
     container.innerHTML = '';
     
@@ -22,34 +17,26 @@ numImagesSelect.addEventListener('change', function(event) {
         fileInput.addEventListener('change', function(event) {
             const file = event.target.files[0];
             
-          
             if (!file.type.startsWith('image/')) {
                 return;
             }
-            
           
             const img = document.createElement('img');
             img.src = URL.createObjectURL(file);
             img.classList.add('w-full', 'max-w-xs', 'p-2', 'border', 'border-orange-300', 'rounded-lg', 'shadow-md', 'my-2');
             
-          
             container.replaceChild(img, fileInput);
-            
-           
             container.scrollTop = container.scrollHeight;
         });
-        
         container.appendChild(fileInput);
     }
 });
 
 
 /* mostrar imagenes tanto cental como superio derecha */ 
-
-
- document.addEventListener('DOMContentLoaded', function() {
-    const dropzoneInput = document.getElementById('dropzone-file');
-    const previewContainer = document.getElementById('preview');
+document.addEventListener('DOMContentLoaded', function() {
+const dropzoneInput = document.getElementById('dropzone-file');
+const previewContainer = document.getElementById('preview');
 
     dropzoneInput.addEventListener('change', function() {
         const file = this.files[0];
@@ -69,9 +56,9 @@ numImagesSelect.addEventListener('change', function(event) {
 });
  
 
- document.addEventListener('DOMContentLoaded', function() {
-    const dropzoneInput = document.getElementById('dropzone-imagenE');
-    const previewContainer = document.getElementById('vistaEmpresa');
+document.addEventListener('DOMContentLoaded', function() {
+const dropzoneInput = document.getElementById('dropzone-imagenE');
+const previewContainer = document.getElementById('vistaEmpresa');
 
     dropzoneInput.addEventListener('change', function() {
         const file = this.files[0];
@@ -149,15 +136,12 @@ numImagesSelect.addEventListener('change', function(event) {
     });
   }); */
 
-
-
-  //Dom de checklist de tallas
-
-  document.addEventListener('DOMContentLoaded', function () {
-    const button = document.getElementById('dropdownSearchButton');
-    const menu = document.getElementById('dropdownSearch');
-    const iconOpen = document.getElementById('dropdownIconOpen');
-    const iconClosed = document.getElementById('dropdownIconClosed');
+//Dom de checklist de tallas
+document.addEventListener('DOMContentLoaded', function () {
+const button = document.getElementById('dropdownSearchButton');
+const menu = document.getElementById('dropdownSearch');
+const iconOpen = document.getElementById('dropdownIconOpen');
+const iconClosed = document.getElementById('dropdownIconClosed');
 
     // Toggle dropdown visibility on button click
     button.addEventListener('click', function () {
@@ -180,66 +164,67 @@ numImagesSelect.addEventListener('change', function(event) {
 });
 
 
-    // Dom de los campos categoria, fit y botones/hebilla
+// Dom de los campos categoria, fit y botones/hebilla
 const categoriaSelect = document.getElementById('Categoria');
-    const fitSelect = document.getElementById('Fit');
-    const fitSelectContainer = document.getElementById('fitSelect');
-    const botonSelect = document.getElementById('Boton');
-    const botonLabel = document.getElementById('botonLabel');
-    const botonSelectContainer = document.getElementById('botonSelect');
+const fitSelect = document.getElementById('Fit');
+const fitSelectContainer = document.getElementById('fitSelect');
+const botonSelect = document.getElementById('Boton');
+const botonLabel = document.getElementById('botonLabel');
+const botonSelectContainer = document.getElementById('botonSelect');
 
-    categoriaSelect.addEventListener('change', function() {
-        const selectedCategoria = categoriaSelect.value;
+categoriaSelect.addEventListener('change', function() {
+const selectedCategoria = categoriaSelect.value;
 
-        if (selectedCategoria === 'moda') {
-            // Mostrar el select de Fit para Moda y llenar opciones
-            fitSelect.innerHTML = `
-                <option value="overol">Overol</option>
-                <option value="pesquero">Pesquero</option>
-                <option value="skinny">Skinny</option>
-            `;
-            fitSelectContainer.classList.remove('hidden');
-        } else if (selectedCategoria === 'pantalon') {
-            // Mostrar el select de Fit para Pantalón y llenar opciones
-            fitSelect.innerHTML = `
-                <option value="slim">Slim</option>
+    if (selectedCategoria === 'moda') {
+        // Mostrar el select de Fit para Moda y llenar opciones
+        fitSelect.innerHTML = `
+            <option value="overol">Overol</option>
+            <option value="pesquero">Pesquero</option>
+            <option value="skinny">Skinny</option>
+        `;
+        fitSelectContainer.classList.remove('hidden');
+    } else if (selectedCategoria === 'pantalon') {
+        // Mostrar el select de Fit para Pantalón y llenar opciones
+        fitSelect.innerHTML = `
+            <option value="slim">Slim</option>
                 <option value="jogger">Jogger</option>
                 <option value="skinny">Skinny</option>
-            `;
-            fitSelectContainer.classList.remove('hidden');
-        } else {
-            // Ocultar el select de Fit si no hay selección válida
-            fitSelectContainer.classList.add('hidden');
-        }
-    });
+        `;
+        fitSelectContainer.classList.remove('hidden');
+    } else {
+        // Ocultar el select de Fit si no hay selección válida
+        fitSelectContainer.classList.add('hidden');
+    }
+});
 
-
+categoriaSelect.addEventListener('change', function() {
+const selectedCategoria = categoriaSelect.value;
     
-    
-
-    categoriaSelect.addEventListener('change', function() {
-        const selectedCategoria = categoriaSelect.value;
-
-        if (selectedCategoria === 'moda') {
-            // Cambiar el título y opciones del select de Botón para Moda
-            botonLabel.textContent = 'Botón/Hebilla';
-            botonSelect.innerHTML = `
-                <option value="boton">Botón</option>
-                <option value="hebilla">Hebilla</option>
-            `;
-            botonSelectContainer.classList.remove('hidden');
-        } else if (selectedCategoria === 'pantalon') {
-            // Restaurar título y opciones originales para Pantalón
-            botonLabel.textContent = 'Botón';
-            botonSelect.innerHTML = `
-                <option value="boton">Botón</option>
-            `;
-            botonSelectContainer.classList.remove('hidden');
-        } else {
-            // Ocultar el select de Botón si no hay selección válida
-            botonSelectContainer.classList.add('hidden');
-        }
-    });
+    if (selectedCategoria === 'moda') {
+        // Cambiar el título y opciones del select de Botón para Moda
+        botonLabel.textContent = 'Botón/Hebilla';
+        botonSelect.innerHTML = `
+            <option value="boton">Botón</option>
+            <option value="hebilla">Hebilla</option>
+        `;
+        botonSelectContainer.classList.remove('hidden');
+    } else if (selectedCategoria === 'pantalon') {
+        // Restaurar título y opciones originales para Pantalón
+        botonLabel.textContent = 'Botón';
+        botonSelect.innerHTML = `
+            <option value="boton">Botón</option>
+        `;
+        botonSelectContainer.classList.remove('hidden');
+    } else {
+        // Ocultar el select de Botón si no hay selección válida
+        botonSelectContainer.classList.add('hidden');
+    }
+});
 
 
-
+function resetearRemoverCampos(){
+    const campoFit = document.getElementById('fitSelect');
+    if(campoFit !== null){
+        campoFit.remove();
+    }
+}
