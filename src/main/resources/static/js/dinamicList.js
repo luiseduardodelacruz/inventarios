dbdeaa8687be1bbd1552ac7bf5df32f5dccffe3d
Aldo-Antonio-27 <aldoantoniobadillovega@gmail.com>
2024-08-13 00:43:43 -0600

    Corrección de Ortografía Paginas Principales, Ver Contraseña Usuarios y Alertas Interfaz Corte.

--- src/main/resources/static/js/dinamicList.js
+++ src/main/resources/static/js/dinamicList.js
@@ -1,3 +1,62 @@
+let alertCount = 0;
+
+// Funcion Alerta
+function showAlert(message) {
+    const alertContainer = document.getElementById('alertContainer');
+        
+    // Crear la Alerta
+    const alert = document.createElement('div');
+    alert.className = 'alert flex flex-col w-full bg-[#e8763d] text-white rounded-xl shadow-xl';
+    alert.style.opacity = 1;
+        
+    // Crear el Mensaje
+    const alertMessage = document.createElement('span');
+    alertMessage.textContent = message;
+    alertMessage.className = 'px-4 py-4 md:px-5 md:py-6 fuente_alerta text-sm md:text-lg lg:text-lg';
+    alert.appendChild(alertMessage);
+        
+    // Crear la Barra de Progreso
+    const progressBarContainer = document.createElement('div');
+    progressBarContainer.className = 'w-full bg-white rounded-b-xl h-2 overflow-hidden';
+
+    const progressBar = document.createElement('div');
+    progressBar.className = 'progress-bar bg-[#db4900] rounded-bl-xl h-full';
+    progressBar.style.width = '100%';
+    progressBarContainer.appendChild(progressBar);
+        
+    alert.appendChild(progressBarContainer);
+        
+    // Agregar la Alerta al Contenedor
+    alertContainer.appendChild(alert);
+        
+    // Administrar el Tiempo de la Alerta
+    let alertDuration = 5000;
+    let startTime = Date.now();
+        
+    const interval = setInterval(() => {
+        const elapsedTime = Date.now() - startTime;
+        const progress = (elapsedTime / alertDuration) * 100;
+        progressBar.style.width = `${0 + progress}%`;
+    
+        if (elapsedTime >= alertDuration) {
+            clearInterval(interval);
+            fadeOutAlert(alert);
+        }
+    }, 50);
+}
+
+// Funcion Desaparecer Alerta
+function fadeOutAlert(alert) {
+    const fadeInterval = setInterval(() => {
+        if (alert.style.opacity > 0) {
+            alert.style.opacity -= 0.1;
+        } else {
+            clearInterval(fadeInterval);
+            alert.remove();
+        }
+    }, 50);
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
     const cantidadTallas = document.getElementById('cantidadTallas');
     const cantidadDobleces = document.getElementById('cantidadDobleces');
@@ -127,18 +186,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     function validarNumeroNoNegativo(event) {
         const input = event.target;
-        if (parseFloat(input.value) < 0) {
+        if (parseFloat(input.value) <= 0) {
             input.value = '';
-            alert('El valor no puede ser negativo.');
+            showAlert(`El valor no puede ser un numero negativo ni igual a 0.`);
         }
     }
 
     function validarNumeroEnteroNoNegativo(event) {
         const input = event.target;
         const valor = input.value;
-        if (!/^\d+$/.test(valor) || parseFloat(valor) < 0) {
+        if (!/^\d+$/.test(valor) || parseFloat(valor) <= 0) {
             input.value = '';
-            alert('El valor debe ser un número entero no negativo.');
+            showAlert(`El valor debe ser un número entero no negativo y mayor a 0.`);
         }
     }
 
